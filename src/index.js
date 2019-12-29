31ae02bd8773c0c00fd0a38969fb786766446281
Ben <ben.speakman@peakon.com>
2019-12-29 03:21:49 +0000

    Add listeners

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import Radio from './components/radio';
 document.addEventListener('DOMContentLoaded', () => {
     new Radio(document.getElementById('audio'), document.getElementById('renderer'), document.getElementById('button'));
 });
+
+fetch('https://radio.fam.wtf/status-json.xsl', { mode: 'no-cors' }).then(blob => blob.json())
+    .then((response) => {
+        document.getElementById('listeners').innerHTML = response.icestats.source.listeners;
+    });
