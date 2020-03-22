249965232dbd3295962586c9858f80e5e3ff4efb
Ben <ben.speakman@peakon.com>
2020-03-22 11:58:11 +0000

    Lint and add travis config

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import Radio from "./components/radio";
+import Radio from './components/radio';
 
 // Initialize radio
-document.addEventListener("DOMContentLoaded", () => {
-  new Radio(
-    document.getElementById("audio"),
-    document.getElementById("renderer"),
-    document.getElementById("button")
+document.addEventListener('DOMContentLoaded', () => {
+  const radio = new Radio(
+    document.getElementById('audio'),
+    document.getElementById('renderer'),
   );
+
+  radio.init(document.getElementById('button'));
 });
 
 // Checks if var is object
@@ -14,46 +15,42 @@ function isObject(a) {
   return !!a && a.constructor === Object;
 }
 
-// renders the lisntener stats
+// renders the listener stats
 function renderStats(response) {
-  var listenerCount = 0;
+  let listenerCount = 0;
   // only one stream, continue as normal
-  if (typeof response.icestats.source.listeners !== "undefined") {
+  if (typeof response.icestats.source.listeners !== 'undefined') {
     listenerCount = response.icestats.source.listeners;
   }
 
   // more than one stream has been listed
   if (
-    typeof response.icestats.source[0] !== "undefined" &&
-    isObject(response.icestats.source[0])
+    typeof response.icestats.source[0] !== 'undefined' && isObject(response.icestats.source[0])
   ) {
-    // loop through the streams
-    response.icestats.source.forEach(item => {
-      // add up all the listeners
-      listenerCount += item.listeners;
-    });
+    // loop through the streams add up all the listeners
+    listenerCount = response.icestats.source.reduce((total, source) => total + source.listeners, 0);
   }
 
   // Update the listener count
-  document.getElementById("listeners").innerHTML = listenerCount;
+  document.getElementById('listeners').innerHTML = listenerCount;
 }
 
 // polls server for stats
 function statsLoop() {
   // Promise resolves when 200 happens
-  var networkPromise = fetch("https://radio.fam.wtf/status-json.xsl", {
-    mode: "no-cors"
+  const networkPromise = fetch('https://radio.fam.wtf/status-json.xsl', {
+    mode: 'no-cors',
   })
     .then(blob => blob.json())
     .then(response => renderStats(response));
 
   // Promise resolves after 10 seconds
-  var timeOutPromise = new Promise(function(resolve, reject) {
+  const timeOutPromise = new Promise((resolve) => {
     setTimeout(resolve, 10000);
   });
 
   // Once all promises have resolved
-  Promise.all([networkPromise, timeOutPromise]).then(function(values) {
+  Promise.all([networkPromise, timeOutPromise]).then(() => {
     // Repeat
     statsLoop();
   });
